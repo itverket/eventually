62b1f0163ab5d9a7bf469be3dded6e9fc779cfcb
Sindre Berge <sindre.n.berge@gmail.com>
2018-03-17 10:18:47 +0100

    loading animations

--- web/eventually-app/src/app/pipes/filter.pipe.ts
+++ web/eventually-app/src/app/pipes/filter.pipe.ts
@@ -8,14 +8,17 @@ export class FilterPipe implements PipeTransform {
 
   transform(events: IEvent[], filterBy: string): IEvent[] {
 
-    if(!filterBy){
+    if (!filterBy || filterBy.length < 3) {
       return events;
     }
-    
+    var filterWithRemovedWhitespaces  = filterBy.toLowerCase().trim().replace(/  +/g, ' ');
+
+    var filters = filterWithRemovedWhitespaces.split(" ").filter(x => x.length > 2);
+
     return events.filter(x =>
-      x.title.toLowerCase().indexOf(filterBy) != -1 ||
-        x.location.indexOf(filterBy) != -1  ||
-        x.description.indexOf(filterBy) != -1
+    filters.some(filter => x.title.toLowerCase().indexOf(filter) != -1) ||
+    filters.some(filter => x.location.toLowerCase().indexOf(filter) != -1) ||
+    filters.some(filter => x.description.toLowerCase().indexOf(filter) != -1)
     )
   }
 }
