b07a8a9409cdeb5aead6998bc6acd4230070f8d5
Anders Kofoed <anders.kfd@gmail.com>
2018-03-18 10:33:32 +0100

    Add feedback service initial solution

---
+++ feedback-service/src/Api/Form/FormController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NServiceBus;
+
+namespace Api.Form
+{
+    [Route("api/form")]
+    public class FormController : Controller
+    {
+        private readonly ILogger<FormController> _logger;
+        private readonly IEndpointInstance _endpoint;
+
+        public FormController(IEndpointInstance endpoint, ILoggerFactory logFactory)
+        {
+            _endpoint = endpoint;
+            _logger = logFactory.CreateLogger<FormController>();
+        }
+
+        /// <summary>
+        /// Fetches all feedback forms
+        /// </summary>
+        [HttpGet]
+        public IEnumerable<string> GetForms()
+        {
+            _logger.LogInformation("Request for forms received");
+            return new List<string>();
+        }
+
+        /// <summary>
+        /// Fetches specified form by event id 
+        /// </summary>
+        /// <param name="eventId">Id of event the form is connected to</param>   
+        [HttpGet("{eventId}")]
+        public string GetForm(Guid eventId)
+        {
+            _logger.LogInformation("Request form connected to  {EventId} received!", eventId);
+            return $"This is a form connected to event with id {eventId}";
+        }
+    }
+}
